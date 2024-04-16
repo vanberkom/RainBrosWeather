0d5f55a50ebc3f6dae3bd6cacea97daf42fe5b6c
vanberkom <vanberkom.luke@gmail.com>
2024-04-16 16:54:21 -0500

    Added base navbar with searchbar.

--- weather_website/weather_website/urls.py
+++ weather_website/weather_website/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', 'base.html'),
+    path('weatherUpdate/', include('weatherUpdate.urls')),
+    path('', RedirectView.as_view(url='weatherUpdate/')),
 ]

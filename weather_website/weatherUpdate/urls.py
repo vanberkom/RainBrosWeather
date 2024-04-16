dc574bbae69178f79e18c51321ce266c637acc3f
vanberkom <vanberkom.luke@gmail.com>
2024-04-16 17:36:17 -0500

    Added links and index page

--- weather_website/weatherUpdate/urls.py
+++ weather_website/weatherUpdate/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+from django.views.generic import RedirectView
+
 urlpatterns = {
     path('', views.index, name='index'),
+    path('base/', views.base, name='base'),
+    path('base/index/', RedirectView.as_view(pattern_name='index', permanent=True)),
 }
--- weather_website/weatherUpdate/views.py
+++ weather_website/weatherUpdate/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {}
+    return render(request, 'index.html', context=context)
+
+def base(request):
+    context = {}
     return render(request, 'base.html', context=context)
+
+

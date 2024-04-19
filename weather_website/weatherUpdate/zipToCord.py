abed849b6b0a76d893df3595d16044b3fd559f59
vanberkom <vanberkom.luke@gmail.com>
2024-04-18 20:02:49 -0500

    Added code to convert zip to long, lat, and city.

---
+++ weather_website/weatherUpdate/zipToCord.py
@@ -0,0 +1,25 @@
+# Had to install this with 'pip install uszipcode'
+from uszipcode import SearchEngine
+
+# Used to convert zipcode to a city
+def ZipToCity(zip_code):
+    search = SearchEngine()
+    zipcode = search.by_zipcode(zip_code)
+    
+    if zipcode:
+        city = zipcode.major_city
+        latitude = zipcode.lat
+        longitude = zipcode.lng
+        return city, latitude, longitude
+    else:
+        return "City not found", None, None
+
+
+# Testing with Fargo
+zip = '58401'
+city, lat, long  = ZipToCity(zip)
+
+if lat and long:
+    print(str(lat))
+    print(str(long))
+    print(str(city))

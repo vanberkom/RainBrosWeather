7f3ca3f03952e00a2125e7f7c84cc5f4cbe953d1
Zane Jude <zjude04@gmail.com>
2024-05-03 19:29:09 -0500

    logout working

--- weather_website/account/views.py
+++ weather_website/account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from .models import Account
 from weatherUpdate.views import index
@@ -86,7 +86,7 @@ def login(request):
     
     return render(request, 'login.html')
 
-def logout(request):
+def logout_view(request):
     logout(request)
     messages.info(request, "Logged out successfully!")
     return redirect('index')
